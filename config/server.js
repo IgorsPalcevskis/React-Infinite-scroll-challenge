d24046b43dcfb0be91dfc40b3b2657f5381083fd
Igors Palcevskis <Igors.Palcevskis@gmail.com>
2019-02-21 23:07:22 +0000

    initial commit \ react app setup and localhost server-side configured

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || '88be43ea6a41c0f768213354a6976a773736d2ba1a5a847342cb5a42906f8029',
+    SECRET: process.env.SECRET || 'dae2d8ee92496618e11f8ac7238f2518240e43f189b4fce372df413ce5227981',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
